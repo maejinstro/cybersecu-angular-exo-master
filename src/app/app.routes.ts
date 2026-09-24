344b0ae105c2c59794d82b128b3e61af82fef397
Chrtsophe Chanteux <chanteux.titof@gmail.com>
2026-09-24 18:38:02 +0200

    Add Guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { adminGuard } from '../guards/admin-guard';
 
 export const routes: Routes = [
     { path : '' , loadComponent: () => import('../features/home/home').then(c => c.Home)},
@@ -7,8 +8,8 @@ export const routes: Routes = [
     { path : 'user-details/:id' , loadComponent: () => import('../features/user/user-details/user-details').then(c => c.UserDetails)},
     { path : 'user-update/:id' , loadComponent: () => import('../features/user/user-update/user-update').then(c => c.UserUpdate)},
     { path : 'item-list' , loadComponent: () => import('../features/item-feature/item-list/item-list').then(c => c.ItemList)},
-    { path : 'item-add' , loadComponent: () => import('../features/item-feature/item-add/item-add').then(c => c.ItemAdd)},
+    { path : 'item-add' , canActivate: [adminGuard], loadComponent: () => import('../features/item-feature/item-add/item-add').then(c => c.ItemAdd)},
     { path : 'item-details/:id' , loadComponent: () => import('../features/item-feature/item-details/item-details').then(c => c.ItemDetails)},
-    { path : 'item-update/:id' , loadComponent: () => import('../features/item-feature/item-update/item-update').then(c => c.ItemUpdate)},
+    { path : 'item-update/:id' , canActivate: [adminGuard], loadComponent: () => import('../features/item-feature/item-update/item-update').then(c => c.ItemUpdate)},
     { path : '**' , loadComponent: () => import('../features/home/home').then(c => c.Home)},
 ];
